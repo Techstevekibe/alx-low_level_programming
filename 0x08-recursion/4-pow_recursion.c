4b41823a85552140ed563e1560a6ee71d7676908
Your Name <you@example.com>
2023-10-25 05:41:21 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <math.h>
 /**
  *  * _pow_recursion - func that returns the value of x raised to the power of y
@@ -15,4 +15,4 @@ int _pow_recursion(int x, int y)
 			else if (y == 0)
 						return (1);
 				return (x * _pow_recursion(x, y - 1));
-}
+}X
