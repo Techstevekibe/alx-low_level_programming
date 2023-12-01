3cdb92a250d8ecf5278653fb310a421197b7356e
Your Name <you@example.com>
2023-12-01 07:46:32 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- * Return: If the function fails - -1.
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ * Return: If the function fails
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int o, w, len = 0;
 if (filename == NULL)
@@ -15,9 +15,9 @@ if (text_content != NULL)
 for (len = 0; text_content[len];)
 len++;
 }
-o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+o = open(filename, O_WRONLY | O_APPEND);
 w = write(o, text_content, len);
-if (0 == -1 || w == -1)
+if (o == -1 || w == -1)
 return (-1);
 close(o);
 return (1);
